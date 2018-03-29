c2dddfd3958e53eb94776b956d9af36f71f1bb7d
alexanderyakovenko <yakovenko5893@gmail.com>
2018-03-29 16:18:57 +0300

    worked

--- VideoPhotoCamera/ViewController.swift
+++ VideoPhotoCamera/ViewController.swift
@@ -7,18 +7,78 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    
+    //
+    var playerView = AVPlayer()
+    var playerController = AVPlayerViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //playVideo()
+        
+        
+        /*
+        guard let url = URL(string: "https://www.youtube.com/watch?v=_qCrK6ppQZ4") else {
+            return
+        }
+        let player = AVPlayer(url: url)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        viewForPlayer.layer.addSublayer(playerLayer)
+        player.play()
+        */
+        
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func play(_ sender: Any) {
+        playVideo()
+    }
+    
+
+    func playVideo() {
+        
+        // https://drive.google.com/open?id=12Xx9n8FKqUtYhTXAYybaIclfQ1ZkKZF3
+        
+        let videoURL = URL(string: "https://drive.google.com/open?id=12Xx9n8FKqUtYhTXAYybaIclfQ1ZkKZF3")
+        
+        var myUrl = "http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v"
+        myUrl = myUrl.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed)!
+        
+        let url = URL(string: myUrl)
+        
+        playerView = AVPlayer(url: url! as URL)
+        
+        playerController.player = playerView
+        
+        self.present(playerController, animated: true) {
+            self.playerController.player?.play()
+        }
+        
+        //self.addChildViewController(playerController)
+        
+        //**Add your view Frame**
+        //playerController.view.frame = viewForPlayer.frame
+        playerController.view.frame = view.bounds
+        
+        //**Add sub view in your view**
+        view.addSubview(playerController.view)
+        
+        //player.play()
+    }
+    
 
 
 }
